f49d7ff33d089ec79d2b761c657835e8db638047
alumno <alumno@A209P33.informatica.cifpaviles.pa>
2019-12-03 18:14:23 +0100

    Examen diciembre Ejercicio 3 resuelto(?)

--- src/fp/daw/examen/Ejercicio3.java
+++ src/fp/daw/examen/Ejercicio3.java
@@ -1,19 +1,39 @@
 package fp.daw.examen;
 
+import java.util.Scanner;
+
 public class Ejercicio3 {
 
-	/* 3 puntos
+	/*
+	 * 3 puntos
 	 * 
-	 * Decimos que un número entero positivo es guay si puede obtenerse como suma de dos o más números
-	 * enteros consecutivos. Por ejemplo, 3 (=1+2), 5 (=2+3), 6(=1+2+3), son números guays.
+	 * Decimos que un número entero positivo es guay si puede obtenerse como suma de
+	 * dos o más números enteros consecutivos. Por ejemplo, 3 (=1+2), 5 (=2+3),
+	 * 6(=1+2+3), son números guays.
 	 * 
-	 * Escribir en el método main un programa que lea un número entero positivo e indique si éste es guay.
+	 * Escribir en el método main un programa que lea un número entero positivo e
+	 * indique si éste es guay.
 	 * 
 	 * 
 	 */
-	
-	public static void main(String[] args) {
-		
-	}
 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		boolean test=true;
+		System.out.print("Introduce un número entero");
+		
+		int num = in.nextInt();
+
+		for (int i = 1; i <= num; i++) {
+			test = false;
+			for (int j = 2; j < ((i / 2) + 1); j++) {
+				if (i % j == 0) {
+					test = true;
+				}
+			}
+			}if (test) {
+				System.out.print(" Es perfecto");
+		}
+	}
 }
+
